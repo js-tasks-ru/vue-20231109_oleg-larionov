ceef87722f93fde6e93d2a18317590b9068b035c
Олег Ларионов <m2008656@edu.misis.ru>
2023-11-15 17:39:10 +0300

    tasks 1.1 & 1.2

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,15 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+createApp({
+	data() {
+		return {
+			counter: 0,
+		};
+	},
+	methods: {
+		increaseCounter() {
+			this.counter++;
+		},
+	},
+}).mount('#app');
--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,34 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+createApp({
+    data() {
+        return {
+            firstOperand: 0,
+            secondOperand: 0,
+            action: '',
+            result: 0
+        };
+    },
+    methods: {
+        calculateResult() {
+            if (this.action === 'sum') { this.result = this.firstOperand + this.secondOperand }
+            else if (this.action === 'multiply') { this.result = this.firstOperand * this.secondOperand }
+            else if (this.action === 'subtract') { this.result = this.firstOperand - this.secondOperand }
+            else if (this.action === 'divide') { this.result = this.firstOperand / this.secondOperand }
+        },
+    },
+    watch: {
+        action() {
+            this.calculateResult()
+        },
+
+        firstOperand() {
+            this.calculateResult()
+        },
+        
+        secondOperand() {
+            this.calculateResult()
+        }
+    }
+}).mount('#app');
